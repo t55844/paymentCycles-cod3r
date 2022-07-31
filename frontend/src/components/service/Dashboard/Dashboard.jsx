66fd4075929021801b77a168ed90d132231353df
thiago <thiagosoares111@yahoo.com.br>
2022-07-31 13:40:26 -0300

    criando Dashboard e  algumas atualizações de responsividade

--- frontend/src/components/service/Dashboard/Dashboard.jsx
+++ frontend/src/components/service/Dashboard/Dashboard.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import '../../service/service.css'
+import ValueBox from "../ValueBox/ValueBox";
 
 export default props => {
     return (
-        <h1>Dashboard</h1>
+        <div className="service-container">
+            <div className="title-container">
+                <h2 >Dahsboard</h2>
+                <h3 >Version 1.0</h3>
+            </div>
+            <div className="service-content">
+                <ValueBox
+                    value="1000"
+                    valueDescription='Total de Créditos'
+                    backgroundColor='#2bd073'
+                    icon="https://img.icons8.com/external-solidglyph-m-oki-orlando/32/000000/external-Parthenon-landmark-solidglyph-m-oki-orlando.png"
+                ></ValueBox>
+                <ValueBox
+                    value="950"
+                    valueDescription='Total de Debitos'
+                    backgroundColor='#eba467'
+                    icon="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAA3klEQVRoge2YQQrCMBBFX8WleDHbU1m8lfYAehrTfV1ooZUJtlbJGP+DDyWr/5gkhYAQQgghTCqgAVqgc5YWOAHlK4mDg7JTU8ckKgfl5sacTOOg2Nwc+/LFQCQAG8vQMQHYwlikS9NlMQXAKnWLT7E21gpjzSOjHZTNRCTijWxErMP+k9dwNhPJRsTaWhZL/y1f365/NxH3F0A2E5GINyTiDYl4QyLeGIq0yVq8z7X/GIqcExRZysVaLEn/cjg3u5hh7aDc1OxjEj0l9zfV4KDsc8KjW3QSQgghxF9zAyCnNlXJ8y95AAAAAElFTkSuQmCC"
+                ></ValueBox>
+                <ValueBox
+                    value="50"
+                    valueDescription='Valor Consolidado'
+                    backgroundColor='#36678b'
+                    icon="https://img.icons8.com/ios-filled/50/000000/receive-change.png"
+                ></ValueBox>
+            </div>
+        </div>
     )
 }
--- frontend/src/components/service/PaymentCycles/PaymentCycles.jsx
+++ frontend/src/components/service/PaymentCycles/PaymentCycles.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import '../../service/service.css'
+
 
 export default props => {
     return (
-        <h1>PaymentCycles</h1>
+        <div className="service-container">
+            <div className="title-container">
+                <h2 >PaymentCycles</h2>
+                <h3 >Version 1.0</h3>
+            </div>
+            <div className="service-content"></div>
+        </div>
     )
 }
